ebd28cab2e5f95452f57c49becc4b97d0de6fc54
Azizan <zhiz.nevermind@gmail.com>
2025-03-07 14:36:24 +0700

    if null conditions

--- src/todolist/Todolist.jsx
+++ src/todolist/Todolist.jsx
@@ -4,6 +4,7 @@ export default function Todolist() {
   return (
     <>
       <ul>
+        <Todo text="Melihat Film" isCompleted={false} isDeleted={true} />
         <Todo text="Belajar" isCompleted={false} />
         <Todo text="Menonton TV" isCompleted={true} />
         <Todo text="Mengaji" isCompleted={true} />
