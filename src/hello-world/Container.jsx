029c5ab12690887ad26ede70d7dc7c57cffce5f2
Azizan <zhiz.nevermind@gmail.com>
2025-03-07 14:03:41 +0700

    nested Component

---
+++ src/hello-world/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({ children }) {
+  return (
+    <div>
+      <h1>Azizan Belajar REACT</h1>
+      {children}
+      <footer>
+        <p>2025 Footer</p>
+      </footer>
+    </div>
+  );
+}
