5ff9d628783602e15f4bd237b44dda6726f36146
Azizan <zhiz.nevermind@gmail.com>
2025-03-07 14:32:48 +0700

    todolist fixing ul

--- src/todolist/Todolist.jsx
+++ src/todolist/Todolist.jsx
@@ -3,8 +3,11 @@ import Todo from "./Todo";
 export default function Todolist() {
   return (
     <>
-      <Todo text="Belajar" isCompleted={false} />
-      <Todo text="Menonton TV" isCompleted={true} />
+      <ul>
+        <Todo text="Belajar" isCompleted={false} />
+        <Todo text="Menonton TV" isCompleted={true} />
+        <Todo text="Mengaji" isCompleted={true} />
+      </ul>
     </>
   );
 }
