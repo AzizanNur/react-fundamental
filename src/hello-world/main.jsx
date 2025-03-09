47af3ff53f52036f7e43017392123cc5718fce45
Azizan <zhiz.nevermind@gmail.com>
2025-03-09 10:08:49 +0700

    e.stopPropagation() react

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -5,6 +5,7 @@ import Container from "./Container";
 import Todolist from "../todolist/Todolist";
 import AlertButton from "../button/AlertButton";
 import OnSmash from "../button/OnSmash";
+import Toolbar from "../button/Toolbar";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,6 +17,12 @@ createRoot(document.getElementById("root")).render(
         text="OnSmash"
         OnSmash={() => {
           alert("siap siap ini onSmash");
+        }}
+      />
+      <Toolbar
+        onClick={(e) => {
+          e.stopPropagation();
+          alert("halo ini toolbar");
         }}
       />
     </Container>
