4cf68ba0282278d6f72f114e0d0b38f9a3e8bbef
Azizan <zhiz.nevermind@gmail.com>
2025-03-09 17:12:02 +0700

    counter with function native

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -8,6 +8,7 @@ import OnSmash from "../button/OnSmash";
 import Toolbar from "../button/Toolbar";
 import SearchForm from "../form/SearchForm";
 import SayHelloForm from "../form/SayHelloForm";
+import Counter from "../form/Counter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -28,6 +29,8 @@ createRoot(document.getElementById("root")).render(
           alert("halo ini toolbar");
         }}
       />
+
+      <Counter />
 
       <SayHelloForm />
     </Container>
