9a726f9150af9ee46f0ca4b85e57871febd74bb0
Azizan <zhiz.nevermind@gmail.com>
2025-03-11 14:01:10 +0700

    useRef

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,7 +1,10 @@
+import { useRef } from "react";
+
 export default function AlertButton({ text, message }) {
+  const counter = useRef(0);
   const handleAlert = (e) => {
     console.info(e.target);
-    alert(message);
+    alert(`${message} -- ${counter.current++}`);
   };
   return <button onClick={handleAlert}>{text}</button>;
 }
